b7ad43bc32a2e37128c356884fea68f98df223f5
DEREFERENC3D <DEREFERENC3D@protonmail.com>
2025-11-13 02:15:58 +0100

    redragon/aryaman_k569: animations: Remove duplicated include of "effect_runner_i_custom.h"

--- keyboards/redragon/aryaman_k569/animations/cycle_all_custom.h
+++ keyboards/redragon/aryaman_k569/animations/cycle_all_custom.h
@@ -1,7 +1,6 @@
 #ifdef ENABLE_RGB_MATRIX_CUSTOM_CYCLE_ALL
 RGB_MATRIX_EFFECT(CYCLE_ALL)
 #    ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
-#include "runners/effect_runner_i_custom.h"
 
 static hsv_t CYCLE_ALL_math(hsv_t hsv, uint8_t i, uint8_t time) {
     hsv.h = time;
--- keyboards/redragon/aryaman_k569/animations/cycle_left_right_custom.h
+++ keyboards/redragon/aryaman_k569/animations/cycle_left_right_custom.h
@@ -1,7 +1,6 @@
 #ifdef ENABLE_RGB_MATRIX_CUSTOM_CYCLE_LEFT_RIGHT
 RGB_MATRIX_EFFECT(CYCLE_LEFT_RIGHT)
 #    ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
-#include "runners/effect_runner_i_custom.h"
 
 static hsv_t CYCLE_LEFT_RIGHT_math(hsv_t hsv, uint8_t i, uint8_t time) {
     hsv.h = g_led_config.point[i].x - time;
