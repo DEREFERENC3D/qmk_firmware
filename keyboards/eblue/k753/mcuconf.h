b06ef511bbcad907c132d8ad5d885dff1e394e00
DEREFERENC3D <DEREFERENC3D@protonmail.com>
2026-02-01 16:29:41 +0100

    keyboards: Add E-Blue K753

---
+++ keyboards/eblue/k753/mcuconf.h
@@ -0,0 +1,8 @@
+// Copyright 2026 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define PLATFORM_MCUCONF
+
+#define SN32_PWM_USE_CT16B1 TRUE
---
+++ keyboards/eblue/k753/rules.mk
@@ -0,0 +1,5 @@
+# Copyright 2026 QMK
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+USE_EXCEPTIONS_STACKSIZE = 0x120
+USE_PROCESS_STACKSIZE = 0x1A0
