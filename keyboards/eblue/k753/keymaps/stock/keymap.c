401b3404592b4b617382d5fe74a7c4078b21c5cb
DEREFERENC3D <DEREFERENC3D@protonmail.com>
2026-02-05 00:35:22 +0100

    eblue/k753: Make "stock" keymap from default

---
+++ keyboards/eblue/k753/keymaps/stock/keymap.c
@@ -0,0 +1,51 @@
+// Copyright 2026 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+#include QMK_KEYBOARD_H
+
+enum layer_names {
+    BASE,
+    WINLK,
+    FN,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [BASE] = LAYOUT_fullsize_ansi(
+        KC_ESC,           KC_F1,   KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SCRL, KC_PAUS,
+        KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_INS,  KC_HOME, KC_PGUP, KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,  KC_END,  KC_PGDN, KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_CAPS, KC_A,    KC_S,    KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                             KC_P4,   KC_P5,   KC_P6,
+        KC_LSFT,          KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,          KC_UP,            KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        KC_LCTL, KC_LGUI, KC_LALT,                       KC_SPC,                      KC_RALT, MO(FN),  KC_APP,  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT
+    ),
+    [WINLK] = LAYOUT_fullsize_ansi(
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                            _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,          _______, _______, _______, _______,
+        _______, KC_NO,   _______,                            _______,                            _______, _______, _______, _______, _______, _______, _______, _______, _______
+    ),
+    [FN] = LAYOUT_fullsize_ansi(
+        QK_BOOT,            KC_WFAV, KC_MYCM, KC_MAIL, KC_WHOM, KC_MSTP, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSEL, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, EE_CLR,
+        _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, LM_NEXT, _______, _______, _______, _______, _______, _______,
+        _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                            _______, _______, _______,
+        _______,            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          LM_BRIU,          _______, _______, _______, _______,
+        _______, TG(WINLK), _______,                            _______,                            _______, _______, _______, _______, LM_SPDD, LM_BRID, LM_SPDU, _______, _______
+    ),
+};
+
+#define NUM_LOCK_LED_INDEX 33
+#define CAPS_LOCK_LED_INDEX 58
+#define SCROLL_LOCK_LED_INDEX 14
+#define WIN_LOCK_LED_INDEX 92
+bool led_matrix_indicators_user() {
+    led_t state = host_keyboard_led_state();
+
+    led_matrix_set_value(NUM_LOCK_LED_INDEX, state.num_lock ? 255 : 0);
+    led_matrix_set_value(CAPS_LOCK_LED_INDEX, state.caps_lock ? 255 : 0);
+    led_matrix_set_value(SCROLL_LOCK_LED_INDEX, state.scroll_lock ? 255 : 0);
+    led_matrix_set_value(WIN_LOCK_LED_INDEX, layer_state_is(WINLK) ? 255 : 0);
+
+    return true;
+}
